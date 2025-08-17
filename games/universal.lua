59cdd322d534c459d1b16a91f48fd046ca8e096a
Copium <119388242+Copiums@users.noreply.github.com>
2025-08-16 21:56:39 -0400

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -72,7 +72,7 @@ local function addBlur(parent)
 	blur.Size = UDim2.new(1, 89, 1, 52)
 	blur.Position = UDim2.fromOffset(-48, -31)
 	blur.BackgroundTransparency = 1
-	blur.Image = getcustomasset('rust/assets/new/blur.png')
+	blur.Image = "rbxassetid://14898786664"
 	blur.ScaleType = Enum.ScaleType.Slice
 	blur.SliceCenter = Rect.new(52, 31, 261, 502)
 	blur.Parent = parent

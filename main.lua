04e127ee0dba869234b91f0ea474dda8de2eb5b6
Copium <119388242+Copiums@users.noreply.github.com>
2025-08-21 18:08:31 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,119 +1,129 @@
-repeat task.wait() until game:IsLoaded()
-if shared.rust then shared.rust:Uninject() end
 
--- why do exploits fail to implement anything correctly? Is it really that hard?
-if identifyexecutor then
-	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
-		getgenv().setthreadidentity = nil
-	end
+repeat task.wait() until game:IsLoaded()
+
+if shared.rust then
+    shared.rust:Uninject()
 end
 
-local rust
---[[
-local loadstring: any = function(...)
-        local res: any, err: string? = loadstring(...);
-	    if err and vape then
-		        rust:CreateNotification('Rust', 'Failed to load : '..err, 30, 'alert');
-	    end;
-	    return res;
-end;]]--
+if identifyexecutor then
+    if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
+        getgenv().setthreadidentity = nil
+    end
+end
+
+local function loadstrings(code, name)
+    local fn, err = loadstring(code, name)
+    if not fn then
+        if shared.rust then
+            shared.rust:CreateNotification('Rust', 'Failed to load '..name..' : '..err, 30, 'alert')
+        else
+            error(err)
+        end
+    end
+    return fn
+end
 
 local queue_on_teleport = queue_on_teleport or function() end
 local isfile = isfile or function(file)
-	local suc, res = pcall(function()
-		return readfile(file)
-	end)
-	return suc and res ~= nil and res ~= ''
+    local suc, res = pcall(readfile, file)
+    return suc and res ~= nil and res ~= ''
 end
-local cloneref = cloneref or function(obj)
-	return obj
-end
+local cloneref = cloneref or function(obj) return obj end
+
 local playersService = cloneref(game:GetService('Players'))
 
 local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/'..select(1, path:gsub('rust/', '')), true)
-		end)
-		if not suc or res == '404: Not Found' then
-			error(res)
-		end
-		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after rust updates.\n'..res
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
+    if not isfile(path) then
+        local suc, res = pcall(function()
+            return game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/'..select(1, path:gsub('rust/', '')), true)
+        end)
+        if not suc or res == '404: Not Found' then
+            error(res)
+        end
+        if path:find('.lua') then
+            res = '--This watermark is used to delete the file if cached.\n'..res
+        end
+        writefile(path, res)
+    end
+    return (func or readfile)(path)
 end
 
 local function finishLoading()
-	rust.Init = nil
-	rust:Load()
-	task.spawn(function()
-		repeat
-			rust:Save()
-			task.wait(10)
-		until not rust.Loaded
-	end)
+    rust.Init = nil
+    rust:Load()
 
-	local teleportedServers
-	rust:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-		if (not teleportedServers) and (not shared.RustIndependent) then
-			teleportedServers = true
-			local teleportScript = [[
-				shared.rustreload = true
-				if shared.RustDeveloper then
-					loadstring(readfile('rust/loader.lua'), 'loader')()
-				else
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/loader.lua', true), 'loader')()
-				end
-			]]
-			if shared.RustDeveloper then
-				teleportScript = 'shared.RustDeveloper = true\n'..teleportScript
-			end
-			if shared.RustCustomProfile then
-				teleportScript = 'shared.RustCustomProfile = "'..shared.RustCustomProfile..'"\n'..teleportScript
-			end
-			rust:Save()
-			queue_on_teleport(teleportScript)
-		end
-	end))
+    task.spawn(function()
+        repeat
+            rust:Save()
+            task.wait(10)
+        until not rust.Loaded
+    end)
 
-	if not shared.rustreload then
-		if not rust.Categories then return end
-		if rust.Categories.Main.Options['GUI bind indicator'].Enabled then
-			rust:CreateNotification('Finished Loading', rust.RustButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(rust.Keybind, ' + '):upper()..' to open GUI', 5)
-		end
-	end
+    local teleportedServers
+    rust:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
+        if (not teleportedServers) and (not shared.RustIndependent) then
+            teleportedServers = true
+            local teleportScript = [[
+                shared.rustreload = true
+                if shared.RustDeveloper then
+                    loadstring(readfile('rust/loader.lua'), 'loader')()
+                else
+                    loadstring(game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+                end
+            ]]
+            if shared.RustDeveloper then
+                teleportScript = 'shared.RustDeveloper = true\n'..teleportScript
+            end
+            if shared.RustCustomProfile then
+                teleportScript = 'shared.RustCustomProfile = "'..shared.RustCustomProfile..'"\n'..teleportScript
+            end
+            rust:Save()
+            queue_on_teleport(teleportScript)
+        end
+    end))
+
+    if not shared.rustreload then
+        if not rust.Categories then return end
+        if rust.Categories.Main.Options['GUI bind indicator'].Enabled then
+            rust:CreateNotification(
+                'Finished Loading',
+                rust.RustButton and 'Press the button in the top right to open GUI' or
+                'Press '..table.concat(rust.Keybind, ' + '):upper()..' to open GUI',
+                5
+            )
+        end
+    end
 end
 
 if not isfile('rust/profiles/gui.txt') then
-	writefile('rust/profiles/gui.txt', 'rise')
+    writefile('rust/profiles/gui.txt', 'rise')
 end
 local gui = readfile('rust/profiles/gui.txt')
-
 if not isfolder('rust/assets/'..gui) then
-	makefolder('rust/assets/'..gui)
+    makefolder('rust/assets/'..gui)
 end
+
 rust = loadstrings(downloadFile('rust/guis/'..gui..'.lua'), 'gui')()
 shared.rust = rust
 
 if not shared.RustIndependent then
-	loadstrings(downloadFile('rust/games/universal.lua'), 'universal')()
-	if isfile('rust/games/'..game.PlaceId..'.lua') then
-		loadstrings(readfile('rust/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-	else
-		if not shared.RustDeveloper then
-			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
-			end)
-			if suc and res ~= '404: Not Found' then
-				loadstrings(downloadFile('rust/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
-			end
-		end
-	end
-	finishLoading()
+    loadstrings(downloadFile('rust/games/universal.lua'), 'universal')()
+    
+    if isfile('rust/games/'..game.PlaceId..'.lua') then
+        loadstrings(readfile('rust/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))()
+    else
+        if not shared.RustDeveloper then
+            local suc, res = pcall(function()
+                return game:HttpGet('https://raw.githubusercontent.com/0xEIite/rust/'..readfile('rust/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+            end)
+            if suc and res ~= '404: Not Found' then
+                loadstrings(downloadFile('rust/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))()
+            end
+        end
+    end
+    
+    finishLoading()
 else
-	rust.Init = finishLoading
-	return rust
+    rust.Init = finishLoading
+    return rust
 end
